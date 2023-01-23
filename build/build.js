919e5779ffbafcfee8956fe6e44c9612c37b2ea5
i-ght <toph.ght@gmail.com>
2023-01-23 06:06:25 -0700

    growth

---
+++ build/build.js
@@ -0,0 +1,7 @@
+function setup() {
+    console.log("🚀 - Setup initialized - P5 is running");
+    createCanvas(windowWidth, windowHeight);
+}
+function draw() {
+}
+//# sourceMappingURL=build.js.map
---
+++ sketch/sketch.ts
@@ -0,0 +1,13 @@
+
+
+function setup() {
+  console.log("🚀 - Setup initialized - P5 is running");
+
+  createCanvas(windowWidth, windowHeight)
+}
+
+// p5 WILL HANDLE REQUESTING ANIMATION FRAMES FROM THE BROWSER AND WIL RUN DRAW() EACH ANIMATION FROME
+function draw() {
+  
+  
+}
