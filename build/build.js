181d36129ac453f9141f5cc1463265dc25a8ee61
i-ght <toph.ght@gmail.com>
2023-01-23 06:18:25 -0700

    shine forth

--- build/build.js
+++ build/build.js
@@ -1,7 +1,25 @@
-function setup() {
-    console.log("🚀 - Setup initialized - P5 is running");
-    createCanvas(windowWidth, windowHeight);
+function radiateRaindrop(r, a) {
+    translate(-width / 2, -height / 2, 0);
+    beginShape();
+    for (var i = 0; i < 360; i++) {
+        var x = width / 2 + cos(radians(i)) * r;
+        var y = height / 2 + sin(radians(i)) * pow(sin(radians(i) / 2), a) * r;
+        vertex(x, y);
+    }
+    endShape();
 }
+function setup() {
+    createCanvas(640, 480, WEBGL);
+    normalMaterial();
+    debugMode();
+}
+var a = 2.3;
 function draw() {
+    background(200);
+    orbitControl();
+    var b = map(a += 2.3, 0, width, 0, 9);
+    radiateRaindrop(98.6, b);
+    if (a >= width)
+        a = 0;
 }
 //# sourceMappingURL=build.js.map
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,13 +1,37 @@
+/* CREDIT: https://openprocessing.org/sketch/15988/
+   Tear Drop Curve by Michael
+   https://www.youtube.com/watch?v=334kdqpyzXQ
+   Oh Micahel, Oh Jesus, You Know I'm Not To Blame!
+   You Know My Reputation, For Playing A Good Clean Game! */
+function radiateRaindrop(r: number, a: number) {
+  translate(-width/2,-height/2,0);
+  
+  beginShape();
+  for (let i = 0; i < 360; i++) {
+    let x = width/2 + cos(radians(i)) *r;
+    let y = height/2+ sin(radians(i)) * pow(sin(radians(i)/2), a) *r;
+    /*ellipse(x,y, 10,10);
+    point(x,y);*/
+    vertex(x, y);
+  }
 
-
-function setup() {
-  console.log("🚀 - Setup initialized - P5 is running");
-
-  createCanvas(windowWidth, windowHeight)
+  endShape();
 }
 
-// p5 WILL HANDLE REQUESTING ANIMATION FRAMES FROM THE BROWSER AND WIL RUN DRAW() EACH ANIMATION FROME
+function setup() {
+  createCanvas(640, 480, WEBGL);
+  normalMaterial();
+  debugMode();
+}
+
+let a = 2.3;
+
 function draw() {
-  
-  
+  background(200);
+  orbitControl();
+  let b = map(a+=2.3, 0, width, 0, 9);
+  radiateRaindrop(98.6, b);
+
+  if (a >= width)
+    a = 0;  
 }
